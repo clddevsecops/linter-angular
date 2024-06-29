2455c971d22e89138045642153ff8763f11d430c
clddevsecops <157944404+clddevsecops@users.noreply.github.com>
2024-06-29 17:44:09 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -9,5 +9,4 @@ bootstrapApplication(AppComponent, appConfig).catch((err) =>
                                                     
 
                                                     
-                                                    
 );
