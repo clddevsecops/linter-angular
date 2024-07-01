479e2065cc1bd7d15e55a95f606864a35686c1af
clddevsecops <157944404+clddevsecops@users.noreply.github.com>
2024-07-01 16:17:56 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,4 +6,5 @@ import { appConfig } from "./app/app.config";
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err),                       
 
+                                                    
                                                    );
